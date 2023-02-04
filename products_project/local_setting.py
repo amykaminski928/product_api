4a127fdb8f2e0e52ff6986676b48c4b57d4cee47
Amy Kaminski <amykaminski928@gmail.com>
2023-02-03 21:01:26 -0600

    Installed pipfile + Project Packages + Local Settings

---
+++ products_project/local_setting.py
@@ -0,0 +1,14 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-osaoh-wy*f02d*#8der$v#6xp!$la)(6d@xq9xf-z1rey+nqp3'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products_database',
+        'HOST': 'localhost',
+        'PASSWORD': '0liver-El@1se',
+    }
+}
